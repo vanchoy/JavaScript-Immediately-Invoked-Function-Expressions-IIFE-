b5192edf40e90550df53d607dfab92f1c598b856
Ivan Stoyanov <super.ludiq@gmail.com>
2019-02-12 17:53:23 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+function game() {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+game();
+
+(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+}
+)();
+
+// console.log(score); throws an error
+
+
+// For data privacy
+(function (goodLuck) {
+    var score = Math.random() * 10;
+    console.log(score >= 5 - goodLuck);
+})(5); 
